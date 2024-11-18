2e6319ad078f80c9b78d073e32d3b7a1050ee0d9
tamyhelo123 <nunestamily66@gmail.com>
2024-11-18 11:35:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma lingugem de estilização!'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+)
+criaCartao(
+    'Programação',
+    'O que é VSCode?',
+    'O Visual Studio Code é um editor de código com diversas integrações.s'
+)
+criaCartao(
+    'Programação',
+    'O que é GitHub?',
+    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+)
